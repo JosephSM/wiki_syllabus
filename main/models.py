6b6046a9e6d8e0a21484869cc7bd90d48ffc2c2d
Alex Mancheno <alex.mancheno@gmail.com>
2016-10-15 10:29:46 -0400

    started db

---
+++ main/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Professor(models.Model):
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length = 30)
+    username = models.CharField(max_length = 30)
+    password =
